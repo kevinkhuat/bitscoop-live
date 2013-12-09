9052cb6887610b94d1cd25124761b09b2cd12010
Steven Berry <sberry@smokesignal.io>
2013-12-08 21:20:33 -0500

    Adding more resources to the OData API.

--- ografy/apps/core/resources.py
+++ ografy/apps/core/resources.py
@@ -3,20 +3,26 @@ from __future__ import unicode_literals
 from tastypie import fields
 from tastydata.resources import Resource
 
-from ografy.apps.core.models import Entry, User
+from ografy.apps.core.models import Account, Entry, User
 
 
 class UserResource(Resource):
-    #entries = fields.ToManyField('ografy.apps.core.resources.EntryResource', 'entry_set', related_name='entries')
-
     class Meta:
         queryset = User.objects.all()
         allowed_methods = ['get']
 
 
-#class EntryResource(Resource):
-#    user = fields.ForeignKey(UserResource, 'user')
-#
-#    class Meta:
-#        queryset = Entry.objects.all()
-#        allowed_methods = ['get']
+class AccountResource(Resource):
+    user = fields.ForeignKey(UserResource, 'user')
+
+    class Meta:
+        queryset = Account.objects.all()
+        allowed_methods = ['get']
+
+
+class EntryResource(Resource):
+    account = fields.ForeignKey(AccountResource, 'account')
+
+    class Meta:
+        queryset = Entry.objects.all()
+        allowed_methods = ['get']
--- ografy/apps/core/urls.py
+++ ografy/apps/core/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import patterns, url
 
 from tastydata.api import Api as ODataApi
 
-from ografy.apps.core.resources import UserResource
+from ografy.apps.core.resources import AccountResource, EntryResource, UserResource
 from ografy.apps.core import views
 
 odata_v1 = ODataApi(api_name='v1')
 odata_v1.register(UserResource())
+odata_v1.register(AccountResource())
+odata_v1.register(EntryResource())
 
 urlpatterns = patterns('',
     url(r'^/?$', views.index)
