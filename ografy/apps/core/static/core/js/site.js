25bbb7aee49973991093d7b76531ba0274ff2ac7
Steven Berry <sberry@smokesignal.io>
2013-12-15 18:08:44 -0500

    Main page updates.

--- ografy/apps/core/static/core/js/site.js
+++ ografy/apps/core/static/core/js/site.js
@@ -13,21 +13,66 @@ $(document).ready(function() {
 	});
 	
 	$(document).delegate('.steps > a', 'click', function(e) {
-		var href, $this = $(this);
+		var $this = $(this);
 		
 		e.preventDefault();
 		
 		$('.steps > a.active').removeClass('active');
 		$this.addClass('active');
 		
-		href = $this.attr('href');
-		$.waypoints('disable');
-		$(document.body).scrollTo(href, {
+		$(document.body).waypointScroll($this.attr('href'));
+	});
+	
+	
+	$(document).delegate('input[name="password"]', 'change', function(e) {
+		var value = $(this).val();
+		
+		$('input[name="password"]').each(function(i, el) {
+			$(el).val(value);
+		});
+	});
+	
+	
+	$('#sign-up').bind('submit', function(e) {
+		var identifier, $username, $email;
+		
+		e.preventDefault();
+		
+		$username = $('input[name="username"]');
+		$email = $('input[name="email"]');
+		identifier = $('input[name="identifier"]').val();
+		
+		$username.val('');
+		$email.val('');
+		$('.signup').show();
+		
+		if (/^\w+@\w+\.\w+$/.test(identifier)) {
+			$email.val(identifier);
+			$username.focus();
+		}
+		else {
+			$username.val(identifier);
+			$email.focus();
+		}
+		
+		$(document.body).waypointScroll('#welcome');
+	});
+});
+
+
+$.fn.extend({
+	waypointScroll: (function() {
+		var options = {
 			offset: -65,
 			duration: 150,
 			onAfter: function() {
 				$.waypoints('enable');
 			}
-		});
-	});
+		};
+		
+		return function waypointScroll(target) {
+			$.waypoints('disable');
+			$(this).scrollTo(target, options);
+		};
+	})()
 });
