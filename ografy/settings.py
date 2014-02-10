434786cf2f31568040c846d849f6cc16c92ce25b
Steven Berry <sberry@smokesignal.io>
2014-02-09 22:12:16 -0500

    Updating settings in accordance with util package relocation.

--- ografy/settings.py
+++ ografy/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     #'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'ografy.apps.util.middleware.format.RenderFormat',
+    'ografy.util.middleware.format.RenderFormat',
 )
 
 
