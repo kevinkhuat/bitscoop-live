88e1f5b4ee7ef421ab67a628d288abc85220ae8d
Steven Berry <sberry@ografy.io>
2014-03-09 18:10:47 -0400

    Updating keyauth decorators to work with expiration.

--- ografy/apps/keyauth/decorators.py
+++ ografy/apps/keyauth/decorators.py
@@ -14,11 +14,13 @@ def token_required(view_fn, exception=PermissionDenied):
     @wraps(view_fn, assigned=available_attrs(view_fn))
     def _wrapped_view(request, *args, **kwargs):
         token = request.REQUEST.get('token')
-        # TODO: This shouldn't be fetched each request. Something needs to be done to get this into the session.
+        # TODO: Can this be memcached? Probably... take a look into Django cache middleware.
         key = Key.objects.filter(key__exact=token).first()
 
-        if key is None:
+        if key is None or not key.is_valid:
             raise exception
+
+        request.key = key
 
         return view_fn(request, *args, **kwargs)
 
@@ -39,7 +41,7 @@ def key_login(view_fn, persist=False, track=True, exception=PermissionDenied):
 
         # We have a double hit here, see `.backends` module for additional comments/reasoning.
         key = Key.objects.filter(digest__exact=token).first()
-        if key is None:
+        if key is None or not key.is_valid:
             raise exception
 
         login(request, user, key, persist, track)
