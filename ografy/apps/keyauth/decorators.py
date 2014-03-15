4f6826b876555bda41f3e985401578d54df738a6
Steven Berry <sberry@ografy.io>
2014-03-15 16:33:26 -0400

    Removing verbose code. Updating arguments to be consistent with API.

--- ografy/apps/keyauth/decorators.py
+++ ografy/apps/keyauth/decorators.py
@@ -36,15 +36,10 @@ def key_login(view_fn, persist=False, track=True, exception=PermissionDenied):
         token = request.REQUEST.get('token')
 
         user = authenticate(token=token)
-        if user is None or not user.is_verified or not user.is_active:
+        if user is None or not user.is_valid:
             raise exception
 
-        # We have a double hit here, see `.backends` module for additional comments/reasoning.
-        key = Key.objects.filter(digest__exact=token).first()
-        if key is None or not key.is_valid:
-            raise exception
-
-        login(request, user, key, persist, track)
+        login(request, user, persist=persist, track=track)
 
         return view_fn(request, *args, **kwargs)
 
