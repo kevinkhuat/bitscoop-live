9f780209f2b332657ec15c748f55398da685f9b7
Steven Berry <sberry@ografy.io>
2014-03-15 16:32:42 -0400

    Modifying which statistics are tracked in keyauth.

--- ografy/apps/keyauth/__init__.py
+++ ografy/apps/keyauth/__init__.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 from django.contrib.auth import _clean_credentials, login as base_login
 from django.contrib.auth.signals import user_login_failed
 from django.core.exceptions import PermissionDenied
-from django.utils.timezone import now
 
+from ografy.util import get_client_ip
 from ografy.apps.keyauth.backends import TokenBackend, DummyTokenBackend
 from ografy.apps.keyauth.models import Key
+from ografy.apps.keyauth.util import put_address
 
 
 def authenticate(**credentials):
@@ -26,16 +27,12 @@ def authenticate(**credentials):
     user_login_failed.send(sender=__name__, credentials=_clean_credentials(credentials))
 
 
-def login(request, user, key, persist=False, track=False):
+def login(request, user, persist=False, track=False):
     base_login(request, user)
-
-    key.last_login = now()
-    key.login_count += 1
-    key.save()
 
     if not persist:
         request.session.set_expiry(0)
 
     if track:
-        client_ip = request.META['REMOTE_ADDR']
-        key.put_address(client_ip)
+        client_ip = get_client_ip(request)
+        put_address(user, client_ip)
