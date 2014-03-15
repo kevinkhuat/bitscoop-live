22d8151dca8c319e82c2c2087a295644d75b11aa
Steven Berry <sberry@ografy.io>
2014-03-15 16:30:17 -0400

    Updating managers.

--- ografy/apps/keyauth/managers.py
+++ ografy/apps/keyauth/managers.py
@@ -4,9 +4,19 @@ from django.db.models import Manager as BaseManager, Q
 from django.utils.timezone import now
 
 
+class AddressManager(BaseManager):
+    def valid(self):
+        expression = Q(expires__isnull=True) | Q(expires__gt=now())
+
+        return self.filter(expression)
+
+    def invalid(self):
+        return self.filter(expires__lte=now())
+
+
 class KeyManager(BaseManager):
     def valid(self):
-        expression = Q(expires__gt=now()) | Q(expires__isnull=True)
+        expression = Q(expires__isnull=True) | Q(expires__gt=now())
 
         return self.filter(expression)
 
