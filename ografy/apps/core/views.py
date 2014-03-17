1a2e3c3741851dd745759f71ab54c84d4eaa6a85
Steven Berry <sberry@ografy.io>
2014-03-17 15:40:29 -0400

    Adding more views.

--- ografy/apps/core/views.py
+++ ografy/apps/core/views.py
@@ -1,28 +1,41 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
 
 
 def index(request):
-    return render(request, 'core/home/index.html', {
-        'title': 'Ografy'
-    })
+    if request.user.is_authenticated():
+        print('authenticated view')
+        template = 'core/index.html'
+        context = {}
+    else:
+        template = 'core/index.html'
+        context = {}
 
+    return render(request, template, context)
 
 
 class LoginView(View):
     def get(self, request):
-        return render(request, 'core/user/login.html', {
+        return render(request, 'core/login.html', {
             'title': 'Ografy - Login'
         })
 
-    def post(self, request, *args, **kwargs):
-        print(request.POST)
-        print(args)
-        print(kwargs)
+    def post(self, request):
+        if not request.POST.get('remember_me', None):
+            request.session.set_expiry(0)
+        else:
+            request.session.set_expiry(settings.SESSION_LIMIT)
 
-        return render(request, 'core/user/login.html', {
-            'title': 'Ografy - Loggged in!'
-        })
+        return redirect(reverse('core_index'))
+
+
+def logout(request):
+    auth_logout(request)
+
+    return redirect(reverse('core_index'))
