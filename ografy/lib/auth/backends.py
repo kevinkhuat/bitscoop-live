c5f915f6f4cc79cee1d0d799e49c1841b8287731
Steven Berry <sberry@ografy.io>
2014-03-18 00:36:24 -0400

    Updates to auth lib. Should now work with username or handle for identifier login.

--- ografy/lib/auth/backends.py
+++ ografy/lib/auth/backends.py
@@ -1,11 +1,22 @@
 from __future__ import unicode_literals
-from django.contrib.auth.backends import RemoteUserBackend
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend, RemoteUserBackend
 
 from ografy.lib.auth.models import Key
 
 
+class IdentifierBackend(ModelBackend):
+    def authenticate(self, identifier=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        user = UserModel.objects.by_identifier(both=identifier).first()
+
+        if user is not None and user.check_password(password):
+            return user
+
+
 class TokenBackend(RemoteUserBackend):
-    def authenticate(self, token=None):
+    def authenticate(self, token=None, **kwargs):
         key = Key.objects.filter(digest__exact=token).first()
 
         if key is not None and key.is_valid:
