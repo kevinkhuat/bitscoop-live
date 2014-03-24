1bd197ef76f0472b12962796ccb56736955a7836
Steven Berry <sberry@ografy.io>
2014-03-24 04:20:25 -0400

    Drastic modifications to core application. Added support for login/logout flow with appropriate form/model validation.

--- ografy/apps/core/forms.py
+++ ografy/apps/core/forms.py
@@ -1,9 +1,21 @@
 from __future__ import unicode_literals
 
 from django import forms
+from django.contrib.auth import get_user_model
+
+from ografy.lib.xauth.fields import PasswordField
+from ografy.util.forms import Form, ModelForm
 
 
-class LoginForm(forms.Form):
-    identifier = forms.CharField(max_length=150, required=True)
+class LoginForm(Form):
+    identifier = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput())
-    remember_me = forms.BooleanField(widget=forms.CheckboxInput())
+    remember_me = forms.BooleanField(widget=forms.CheckboxInput(), required=False)
+
+
+class SignUpForm(ModelForm):
+    password = PasswordField()
+
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'handle', 'first_name', 'last_name']
