9e649fb7dd4ab0718b3f6910a676e01e17fc7a0d
Steven Berry <sberry@ografy.io>
2014-03-24 04:17:18 -0400

    Updating demo app with additional functionality.

--- ografy/apps/demo/urls.py
+++ ografy/apps/demo/urls.py
@@ -8,5 +8,6 @@ from ografy.apps.demo import views
 urlpatterns = patterns('',
     url(r'^/?$', views.index, name='demo_index'),
     url(r'^/dashboard/?$', views.dashboard, name='demo_dashboard'),
-    url(r'^/debug/?$', views.debug, name='demo_debug'),
+    url(r'^/info/?$', views.info, name='demo_info'),
+    url(r'^/plan/?$', views.plan, name='demo_plan'),
 )
--- ografy/apps/demo/views.py
+++ ografy/apps/demo/views.py
@@ -1,15 +1,28 @@
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from ografy.lib.auth.decorators import key_login
+from ografy.lib.xauth.decorators import key_login
 
 
 @key_login
 def index(request):
-    return render(request, 'demo/index.html', {
-        'title': 'Ografy - Demo'
+    return redirect(reverse('demo_info'))
+
+
+@login_required
+def info(request):
+    return render(request, 'demo/info.html', {
+        'title': 'Ografy - Info'
+    })
+
+
+@login_required
+def plan(request):
+    return render(request, 'demo/plan.html', {
+        'title': 'Ografy - Plan'
     })
 
 
@@ -17,11 +30,4 @@ def index(request):
 def dashboard(request):
     return render(request, 'demo/dashboard.html', {
         'title': 'Ografy - Dashboard'
-    })
-
-
-@login_required
-def debug(request):
-    return render(request, 'demo/index.html', {
-        'title': 'Ografy - Debug'
     })
