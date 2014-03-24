4beca58d6ec4bd54648a957323174c98fb256b44
Steven Berry <sberry@ografy.io>
2014-03-24 04:14:58 -0400

    Making the blog app actually do something.

---
+++ ografy/apps/blog/management/commands/clearblog.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+
+from ografy.apps.blog.models import Article, Tag
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        Article.objects.all().delete()
+        Tag.objects.all().delete()
+
+        # TODO: Remove formatted blog templates from `/templates/blog/posts`.
---
+++ ografy/apps/blog/models.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
+
+from ografy.util.decorators import autoconnect
+
+
+@autoconnect
+class Article(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=120, blank=False, unique=True)
+    slug = models.CharField(max_length=120, blank=False, unique=True, db_index=True)
+    posted = models.DateTimeField()
+    updated = models.DateTimeField()
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def pre_save(self):
+        if self.slug is None or self.slug == '':
+            self.slug = slugify(self.title)
+
+
+@autoconnect
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=50, unique=True, db_index=True)
+
+    articles = models.ManyToManyField(Article, related_name='tags')
+
+    def pre_save(self):
+        if self.slug is None or self.slug == '':
+            self.slug = slugify(self.name)
