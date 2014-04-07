b38dcc5a8156d7f622c1606fb6b01ecc67ee6d16
Steven Berry <sberry@ografy.io>
2014-04-06 21:46:58 -0400

    Adding an update method to ografy.util so that individual properties don't need to be set on model instances.

--- ografy/util/__init__.py
+++ ografy/util/__init__.py
@@ -1,3 +1,13 @@
+from __future__ import unicode_literals
+
+import six
+
+
+def update(obj, **kwargs):
+    for key, value in six.iteritems(kwargs):
+        setattr(obj, key, value)
+
+
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 
