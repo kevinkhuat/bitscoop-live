72d9cca6a33d69ff84701363f0860b939455b057
Kyle Baran <barankyle@yaoo.com>
2014-10-20 16:43:48 -0700

    Updated xauth and core test files

--- ografy/apps/core/tests.py
+++ ografy/apps/core/tests.py
@@ -1,8 +1,8 @@
-from urllib.parse import urlparse
-from django.test import SimpleTestCase
 from django.core.urlresolvers import reverse, resolve
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.test import SimpleTestCase
 from ografy.apps.core.forms import SignUpForm
+from urllib.parse import urlparse
 
 
 class TestCoreViews(SimpleTestCase):
@@ -21,6 +21,17 @@ class TestCoreViews(SimpleTestCase):
                     'last_name': 'Baran',
                     'password': 'Foxtrot1234'
                 }
+            },
+
+            {
+                'valid': False,
+                'signup_form_info': {
+                    'email': 'barankyle5@yahoo.com',
+                    'handle': 'kyletest5',
+                    'first_name': 'Kyle',
+                    'last_name': 'Baran',
+                    'password': 'fox'
+                }
             }
         ]
 
@@ -29,10 +40,13 @@ class TestCoreViews(SimpleTestCase):
                 signup_form = SignUpForm(case.get('signup_form_info'))
                 self.assertEqual(signup_form.is_valid(), True)
                 response = self.client.post(reverse('core_signup'), case.get('signup_form_info'), HTTP_USER_AGENT='Mozilla/5.0')
+                self.assertEqual(response.status_code, 302)
+                self.assertEqual(type(response) is HttpResponseRedirect, True)
                 self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
 
             else:
                 signup_form = SignUpForm(case.get('signup_form_info'))
                 self.assertEqual(signup_form.is_valid(), False)
                 response = self.client.post(reverse('core_signup'), case.get('signup_form_info'), HTTP_USER_AGENT='Mozilla/5.0')
-                self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_signup')
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(type(response) is HttpResponse, True)
--- ografy/apps/xauth/tests.py
+++ ografy/apps/xauth/tests.py
@@ -1,55 +1,127 @@
-from urllib.parse import urlparse
-from django.test import SimpleTestCase
 from django.core.urlresolvers import reverse, resolve
+from django.http import HttpResponse, HttpResponseRedirect
+from django.test import SimpleTestCase
+from ografy.apps.core.forms import SignUpForm
+from urllib.parse import urlparse
 
 
-class LoginTest(SimpleTestCase):
+class TestXAuthViews(SimpleTestCase):
+
+    def test_SingleLogoutView(self):
+        signup_form_info = {
+            'email': 'barankyle10@yahoo.com',
+            'handle': 'kyletest10',
+            'first_name': 'Kyle10',
+            'last_name': 'Baran',
+            'password': 'Foxtrot1234'
+        }
+        signup_form = SignUpForm(signup_form_info)
+        self.assertEqual(signup_form.is_valid(), True)
+
+        response = self.client.post(reverse('core_signup'), signup_form_info, HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.post(reverse('xauth_logout'), HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
 
     def test_LoginView(self):
 
         signup_form_info = {
-            'email': 'barankyl2e@yahoo.com',
-            'handle': 'kyletest2',
-            'first_name': 'Kyle2',
+            'email': 'barankyle11@yahoo.com',
+            'handle': 'kyletest11',
+            'first_name': 'Kyle',
             'last_name': 'Baran',
             'password': 'Foxtrot1234'
         }
-        self.client.post(reverse('core_signup'), signup_form_info, HTTP_USER_AGENT='Mozilla/5.0')
-        self.client.post(reverse('xauth_logout'), HTTP_USER_AGENT='Mozilla/5.0')
+        signup_form = SignUpForm(signup_form_info)
+        self.assertEqual(signup_form.is_valid(), True)
+
+        response = self.client.post(reverse('core_signup'), signup_form_info, HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.post(reverse('xauth_logout'), HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
 
         login_form_info = {
-            'identifier': 'kyletest2',
+            'identifier': 'kyletest11',
             'password': 'Foxtrot1234',
             'remember_me': False
         }
 
         response = self.client.post(reverse('xauth_login'), login_form_info, HTTP_USER_AGENT='Mozilla/5.0')
         self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
 
-
-class SingleLogoutTest(SimpleTestCase):
-
-    def test_logout(self):
+    def test_DoubleLogoutView(self):
         signup_form_info = {
-            'email': 'barankyl3e@yahoo.com',
-            'handle': 'kyletest3',
-            'first_name': 'Kyle3',
+            'email': 'barankyle12@yahoo.com',
+            'handle': 'kyletest12',
+            'first_name': 'Kyle',
             'last_name': 'Baran',
             'password': 'Foxtrot1234'
         }
-        self.client.post(reverse('core_signup'), signup_form_info, HTTP_USER_AGENT='Mozilla/5.0')
-        self.client.post(reverse('xauth_logout'), HTTP_USER_AGENT='Mozilla/5.0')
+        signup_form = SignUpForm(signup_form_info)
+        self.assertEqual(signup_form.is_valid(), True)
 
-        
-class DoubleLogoutTest(SimpleTestCase):
+        response = self.client.post(reverse('core_signup'), signup_form_info, HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
 
-    def test_logout(self):
+        response = self.client.post(reverse('xauth_logout'), HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
+
+        login_form_info = {
+            'identifier': 'kyletest12',
+            'password': 'Foxtrot1234',
+            'remember_me': False
+        }
+
+        response = self.client.post(reverse('xauth_login'), login_form_info, HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.post(reverse('xauth_logout'), HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
+
+    def test_RememberMeView(self):
         signup_form_info = {
-            'email': 'barankyle4@yahoo.com',
-            'handle': 'kyletest4',
-            'first_name': 'Kyle4',
+            'email': 'barankyle13@yahoo.com',
+            'handle': 'kyletest13',
+            'first_name': 'Kyle',
             'last_name': 'Baran',
             'password': 'Foxtrot1234'
         }
-        self.client.post(reverse('core_signup'), signup_form_info, HTTP_USER_AGENT='Mozilla/5.0')
-        self.client.post(reverse('xauth_logout'), HTTP_USER_AGENT='Mozilla/5.0')
+        signup_form = SignUpForm(signup_form_info)
+        self.assertEqual(signup_form.is_valid(), True)
+
+        response = self.client.post(reverse('core_signup'), signup_form_info, HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.post(reverse('xauth_logout'), HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
+
+        login_form_info = {
+            'identifier': 'kyletest13',
+            'password': 'Foxtrot1234',
+            'remember_me': True
+        }
+
+        response = self.client.post(reverse('xauth_login'), login_form_info, HTTP_USER_AGENT='Mozilla/5.0')
+        self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        self.assertEqual(response.status_code, 302)
+
+        #Todo: Figure out where to navigate to to test remember_me functionality
+        response = self.client.post(reverse('core_contact'), HTTP_USER_AGENT='Mozilla/5.0')
+        # self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_contact')
+
+        response = self.client.post(reverse('core_index'), HTTP_USER_AGENT='Mozilla/5.0')
+        # self.assertEqual(resolve(urlparse(response.url).path).view_name, 'core_index')
+        # self.assert
