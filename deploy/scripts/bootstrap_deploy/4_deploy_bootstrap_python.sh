43f956a4b0e4e04f50eb3ed392a00a034f26ee44
William Broza <hegemon.bill@gmail.com>
2014-10-27 11:52:41 -0700

    added pipes to python

--- deploy/scripts/bootstrap_deploy/4_deploy_bootstrap_python.sh
+++ deploy/scripts/bootstrap_deploy/4_deploy_bootstrap_python.sh
@@ -6,14 +6,14 @@ sudo wget https://www.python.org/ftp/python/3.4.1/Python-3.4.1.tar.xz
 sudo xz -d Python-3.4.1.tar.xz
 sudo tar -xvf Python-3.4.1.tar
 cd Python-3.4.1
-sudo ./configure
-sudo make && sudo make altinstall
+yes | sudo ./configure
+yes | sudo make && sudo make altinstall
 export PATH="$PATH:/usr/local/bin/python3.4"
 # curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | sudo python3.4 -
-wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python3.4
-sudo /usr/local/bin/easy_install pip
+yes | wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python3.4
+yes | sudo /usr/local/bin/easy_install pip
 sudo mkdir /ografy
-sudo /usr/local/bin/pip install virtualenv
+yes | sudo /usr/local/bin/pip install virtualenv
 cd /ografy/
 sudo /usr/local/bin/virtualenv /ografy/ografy_env
 source /ografy/ografy_env/bin/activate
