f03c81357b0e2f5bcfb21c1a04b23180c12def5f
kbaran <barankyle@yahoo.com>
2014-10-27 14:19:49 -0700

    Merge branch 'master' of https://github.com/mrhegemon/ografy
    Conflicts:
    	ografy/apps/xauth/fixtures/xauth_test_db.json
    	ografy/apps/xauth/tests.py

--- deploy/scripts/bootstrap_deploy/2_deploy_bootstrap_nginx.sh
+++ deploy/scripts/bootstrap_deploy/2_deploy_bootstrap_nginx.sh
@@ -2,3 +2,5 @@
 # @authors Kyle Baran, Liam Broza
 sudo cp nginx.repo /etc/yum.repos.d/
 yes | sudo yum install nginx
+sudo mkdir /etc/init.d/nginx
+sudo cp -r nginx /etc/init.d/nginx/
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,9 +2,12 @@ import os
 import sys
 
 
-VENV = 'ografy-prod'
-BASEDIR = os.path.join(os.environ['HOME'], 'bin', 'python27', 'environments', VENV)
-INTERP = os.path.expanduser(os.path.join(BASEDIR, 'bin', 'python'))
+# VENV = 'ografy_env'
+# BASEDIR = os.path.join(os.environ['HOME'], 'bin', 'python27', 'environments', VENV)
+# INTERP = os.path.expanduser(os.path.join(BASEDIR, 'bin', 'python'))
+
+BASEDIR = '/ografy/ografy_env/'
+INTERP = '/ografy/ografy_env/bin/python3.4'
 
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
