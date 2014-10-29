4205e1857bbc4587eac14f77f9e296946252db2f
William Broza <hegemon.bill@gmail.com>
2014-10-28 22:54:58 -0700

    Added fix to signup model.

--- ografy/apps/core/forms.py
+++ ografy/apps/core/forms.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
+from ografy.apps.xauth.models import User
 
 from ografy.apps.xauth.fields import PasswordField
 from django.forms import ModelForm
@@ -10,5 +11,7 @@ class SignUpForm(ModelForm):
     password = PasswordField()
 
     class Meta:
-        model = get_user_model()
+        # FIXME: Known 1.7 bug
+        # model = get_user_model()
+        model = User
         fields = ['email', 'handle', 'first_name', 'last_name']
