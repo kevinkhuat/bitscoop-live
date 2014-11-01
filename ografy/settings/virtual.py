9af52d0f0656f383d0c66802ed862a9526dee185
William Broza <hegemon.bill@gmail.com>
2014-10-31 19:20:37 -0700

    Added more logging for VM instances, deploy tools, and better signing certs.

--- ografy/settings/virtual.py
+++ ografy/settings/virtual.py
@@ -9,3 +9,22 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ['.ografy.io', 'localhost']
 
 STATIC_URL = 'https://virtual.static.ografy.io/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.abspath(os.path.join(ROOT_PATH, '..', 'logs', 'debug.log')),
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
