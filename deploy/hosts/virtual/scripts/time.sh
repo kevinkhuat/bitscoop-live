fae26b4fb1d2e95562b5a07572073f40c025593d
Steven Berry <sberry@ografy.io>
2014-11-03 18:47:53 -0800

    Overhauling deploy scripts. Improving organization and cross-compatibility between production and virtual environments.

---
+++ deploy/hosts/virtual/scripts/time.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+# Turn on time server
+sudo chkconfig ntpd on
+# Sync time
+sudo ntpdate pool.ntp.org
--- deploy/scripts/virtual/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm ografy.tar.gz
-rm -rf ografy
-rm passenger-4.0.53.tar.gz
-rm -rf passenger-4.0.53
-rm Python-3.4.2.tgz
-rm -rf Python-3.4.2
