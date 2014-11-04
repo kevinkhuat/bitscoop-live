915f15df8cdfa391370a65a877c72444e675ef88
William Broza <hegemon.bill@gmail.com>
2014-11-03 17:06:06 -0800

    Added Postgres support through vagrant VMs and puppet scripting

--- ografy/settings/development.py
+++ ografy/settings/development.py
@@ -37,3 +37,14 @@ LOGGING = {
         },
     },
 }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ografy_db',
+        'USER': 'ografy_db_user',
+        'PASSWORD': 'foxtrot1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
