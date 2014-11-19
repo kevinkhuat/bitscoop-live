f075572433105848a260af9501ac90d2d546d0cf
William Broza <hegemon.bill@gmail.com>
2014-11-18 18:12:53 -0800

    added small fixes

--- ografy/tests/xauth/urls.py
+++ ografy/tests/xauth/urls.py
@@ -5,10 +5,10 @@ import ografy.tests.xauth.views as views
 
 urlpatterns = patterns('',
     url(r'', include('social.apps.django_app.urls', namespace='social')),
-    url(r'^$', views.home, name='allauth_home'),
-    url(r'^done/$', views.done, name='allauth_done'),
-    url(r'^email/$', views.require_email, name='allauth_require_email'),
-    url(r'^email-sent/', views.validation_sent, name='allauth_validation_sent'),
-    url(r'^login/$', views.home, name='allauth_login'),
-    url(r'^logout/$', views.logout, name='allauth_logout')
+    url(r'^$', views.home, name='test_xauth_home'),
+    url(r'^done/$', views.done, name='test_xauth_done'),
+    url(r'^email/$', views.require_email, name='test_xauth_require_email'),
+    url(r'^email-sent/', views.validation_sent, name='test_xauth_validation_sent'),
+    url(r'^login/$', views.home, name='test_xauth_login'),
+    url(r'^logout/$', views.logout, name='test_xauth_logout')
 )
