dd6299d63d17fe029c81ee81d6c7207be72a341e
Steven Berry <sberry@ografy.io>
2014-11-25 18:39:51 -0800

    Removing django-tastypie dependency as it won't necessarily support MongoDB documents. Updating expressing parsing to accept custom Q expression classes. Reorganizing tastydata code according to the new scope of the library (i.e. a full replacement for tastypie).

--- ografy/apps/tastydata/expressions.py
+++ ografy/apps/tastydata/expressions.py
@@ -3,9 +3,8 @@ import types
 
 from django.db.models import Q
 from django.db.models.constants import LOOKUP_SEP
-from tastypie.exceptions import InvalidFilterError
 
-import ografy.apps.tastydata.util as util
+from ografy.apps.tastydata.exceptions import InvalidFilterError
 
 
 # OData Filter Spec
@@ -68,15 +67,19 @@ class Symbol(object):
 
 
 class Predicate(Symbol):
-    def __init__(self, field, operator, literal):
+    def __init__(self, field, operator, literal, expression_class=Q):
         super(Predicate, self).__init__(field, lbp=0)
         self.field = field
         self.operator = operator
         self.literal = literal
+        self.expression_class = expression_class
 
     def nud(self, *args, **kwargs):
         lhs = '{0}{1}{2}'.format(self.field, LOOKUP_SEP, self.operator)
-        return Q(**{lhs: self.literal})
+
+        return self.expression_class(**{
+            lhs: self.literal
+        })
 
 
 class Infix(Symbol):
@@ -95,7 +98,47 @@ class Prefix(Symbol):
             self.nud = types.MethodType(nud, self)
 
 
-def tokenize(string, inst):
+def _get_literal_value(value):
+    if value == 'True' or value == 'true':
+        return True
+    elif value == 'False' or value == 'false':
+        return False
+    elif value[0] == '\'' and value[-1] == '\'':
+        return value[1:-1]
+
+    try:
+        return int(value)
+    except ValueError:
+        pass
+
+    try:
+        return float(value)
+    except ValueError:
+        pass
+
+    raise InvalidFilterError('Invalid literal: `{0}`'.format(value))
+
+
+def _and_led(self, lhs, expr, **kwargs):
+    return lhs & expr(self.lbp)
+
+
+def _lparen_nud(self, expr, match, **kwargs):
+    value = expr()
+    match(Symbol.get(')'))
+
+    return value
+
+
+def _not_nud(self, expr, **kwargs):
+    return ~expr(self.lbp)
+
+
+def _or_led(self, lhs, expr, **kwargs):
+    return lhs | expr(self.lbp)
+
+
+def tokenize(string, expression_class=Q):
     tokens = ODATA_TOKEN_SPLIT.findall(string)
 
     while len(tokens) > 0:
@@ -104,48 +147,28 @@ def tokenize(string, inst):
         if token in Symbol._cache:
             yield Symbol._cache[token]
         else:
-            raw_field = token
+            field = token
 
             try:
                 operator = tokens.pop(0)
                 raw_literal = tokens.pop(0)
             except IndexError:
-                raise InvalidFilterError('Incomplete predicate format at: `{0}`'.format(raw_field))
+                raise InvalidFilterError('Incomplete predicate format at: `{0}`'.format(field))
 
-            if not operator in ODATA_COMPARERS:
+            if operator not in ODATA_COMPARERS:
                 raise InvalidFilterError('Unexpected filter function: `{0}`'.format(operator))
 
-            field = inst.check_filtering(raw_field)
-            literal = util.get_literal_value(raw_literal)
+            literal = _get_literal_value(raw_literal)
 
-            yield Predicate(field, operator, literal)
+            yield Predicate(field, operator, literal, expression_class=expression_class)
 
     yield Symbol.get('(end)')
 
 
-def or_led(self, lhs, expr, **kwargs):
-    return lhs | expr(self.lbp)
-
-
-def and_led(self, lhs, expr, **kwargs):
-    return lhs & expr(self.lbp)
-
-
-def not_nud(self, expr, **kwargs):
-    return ~expr(self.lbp)
-
-
-def lparen_nud(self, expr, match, **kwargs):
-    value = expr()
-    match(Symbol.get(')'))
-
-    return value
-
-
 if __name__:
     Symbol.register('(end)')
-    Infix.register('or', 50, led=or_led)
-    Infix.register('and', 60, led=and_led)
-    Prefix.register('not', nud=not_nud)
-    Prefix.register('(', 0, nud=lparen_nud)
+    Infix.register('or', 50, led=_or_led)
+    Infix.register('and', 60, led=_and_led)
+    Prefix.register('not', nud=_not_nud)
+    Prefix.register('(', 0, nud=_lparen_nud)
     Prefix.register(')', 0)
