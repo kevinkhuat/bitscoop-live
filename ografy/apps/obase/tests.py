63608d3295957d734b9e88e47c11da52351c7c38
William Broza <hegemon.bill@gmail.com>
2014-11-29 15:21:03 -0800

    fixed test stuff.

--- ografy/apps/obase/tests.py
+++ ografy/apps/obase/tests.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-import json
 import requests
 
-from django.test import SimpleTestCase, TransactionTestCase
+from django.test import SimpleTestCase
 from django.core import serializers
 
 from ografy.apps.obase import api as ObaseApi
@@ -35,10 +34,10 @@ class TestOBase(SimpleTestCase):
         postedData = ObaseApi.Data.post(testData)
         dataId = postedData.id
         gotData = ObaseApi.Data.get(dataId)
-        assert (gotData.data_blob == ["{'cool': 'pants', 'hammer': 'time'}"])
+        self.assertEqual(gotData.data_blob, ["{'cool': 'pants', 'hammer': 'time'}"])
 
-        #Create new test data for testing PUT
-        #This data is not the same as the POST test data
+        # Create new test data for testing PUT
+        # This data is not the same as the POST test data
         testTime = datetime(2011, 5, 15, 15, 12, 40)
         testData = {
             'created': testTime,
@@ -52,41 +51,40 @@ class TestOBase(SimpleTestCase):
         # Check both the 'data_blob' and created fields to ensure this
         putData = ObaseApi.Data.put(dataId, testData)
         gotData = ObaseApi.Data.get(dataId)
-        assert(gotData.data_blob == ["{'top': 'dog'}"])
-        assert(gotData.created == testTime)
+        self.assertEqual(gotData.data_blob, ["{'top': 'dog'}"])
+        self.assertEqual(gotData.created, testTime)
 
-        #Create new test data for testing PATCH
-        #'testTime' will be changed but will not be patched in to make sure
-        #that this variable isn't giving false results
-        #Only the new 'data_blob' will be patched in
+        # Create new test data for testing PATCH
+        # 'testTime' will be changed but will not be patched in to make sure
+        # that this variable isn't giving false results
+        # Only the new 'data_blob' will be patched in
         testTime = datetime.now()
         testData['data_blob'] = ["{'hot': 'dog'}"]
 
-        #PATCH the new 'data_blob' in over the old data
-        #GET the data back and check that 'data_blob'
-        #has been overwitten correctly and that 'created'
-        #hasn't changed
+        # PATCH the new 'data_blob' in over the old data
+        # GET the data back and check that 'data_blob'
+        # has been overwitten correctly and that 'created'
+        # hasn't changed
         patchData = ObaseApi.Data.patch(dataId, testData)
         gotData = ObaseApi.Data.get(dataId)
         assert(gotData.data_blob == ["{'hot': 'dog'}"])
         assert(gotData.created == datetime(2011, 5, 15, 15, 12, 40))
 
-        #DELETE the entry and check that the response is True,
-        #which means the request was successful
+        # DELETE the entry and check that the response is True,
+        # which means the request was successful
         deleteData = ObaseApi.Data.delete(dataId)
         assert(deleteData == True)
 
 
-        #Test the group GET function
-        #It should return an object of type 'QuerySet'
-        #and that object's length should be more than 0
+        # Test the group GET function
+        # It should return an object of type 'QuerySet'
+        # and that object's length should be more than 0
         getGroupData = ObaseApi.Data.get()
-        assert(type(getGroupData) is QuerySet)
         assert(len(getGroupData._result_cache) > 0)
 
     def test_OBase_Event(self):
 
-        #Create initial test data for testing POST
+        # Create initial test data for testing POST
         testTime = datetime.now()
         testData = {
             'created': testTime,
@@ -106,15 +104,15 @@ class TestOBase(SimpleTestCase):
             # 'location':
         }
 
-        #POST the data, save the ID, then GET the data back and check
-        #that some fields are correct, which indicates the POST was successful
+        # POST the data, save the ID, then GET the data back and check
+        # that some fields are correct, which indicates the POST was successful
         postedData = ObaseApi.Event.post(testEvent)
         dataId = postedData.id
         gotData = ObaseApi.Data.get(dataId)
         assert (gotData.data.data_blob == ["{'cool': 'pants', 'hammer': 'time'}"])
 
-        #Create new test data for testing PUT
-        #This data is not the same as the POST test data
+        # Create new test data for testing PUT
+        # This data is not the same as the POST test data
         testTime = datetime(2011, 5, 15, 15, 12, 40)
         testData = {
             'created': testTime,
@@ -122,40 +120,38 @@ class TestOBase(SimpleTestCase):
             'data_blob': ["{'top': 'dog'}"]
         }
 
-        #PUT the new data in over the old data
-        #Don't need to save the ID again since it hasn't changed
-        #GET the data back and check that it has been overwritten correctly
-        #Check both the 'data_blob' and created fields to ensure this
+        # PUT the new data in over the old data
+        # Don't need to save the ID again since it hasn't changed
+        # GET the data back and check that it has been overwritten correctly
+        # Check both the 'data_blob' and created fields to ensure this
         putData = ObaseApi.Data.put(dataId, testData)
         gotData = ObaseApi.Data.get(dataId)
         assert(gotData.data_blob == ["{'top': 'dog'}"])
         assert(gotData.created == testTime)
 
-        #Create new test data for testing PATCH
-        #'testTime' will be changed but will not be patched in to make sure
-        #that this variable isn't giving false results
-        #Only the new 'data_blob' will be patched in
+        # Create new test data for testing PATCH
+        # 'testTime' will be changed but will not be patched in to make sure
+        # that this variable isn't giving false results
+        # Only the new 'data_blob' will be patched in
         testTime = datetime.now()
         testData['data_blob'] = ["{'hot': 'dog'}"]
 
-        #PATCH the new 'data_blob' in over the old data
-        #GET the data back and check that 'data_blob'
-        #has been overwitten correctly and that 'created'
-        #hasn't changed
+        # PATCH the new 'data_blob' in over the old data
+        # GET the data back and check that 'data_blob'
+        # has been overwitten correctly and that 'created'
+        # hasn't changed
         patchData = ObaseApi.Data.patch(dataId, testData)
         gotData = ObaseApi.Data.get(dataId)
         assert(gotData.data_blob == ["{'hot': 'dog'}"])
         assert(gotData.created == datetime(2011, 5, 15, 15, 12, 40))
 
-        #DELETE the entry and check that the response is True,
-        #which means the request was successful
+        # DELETE the entry and check that the response is True,
+        # which means the request was successful
         deleteData = ObaseApi.Data.delete(dataId)
-        assert(deleteData == True)
+        assertEqual(deleteData,True)
 
-
-        #Test the group GET function
-        #It should return an object of type 'QuerySet'
-        #and that object's length should be more than 0
+        # Test the group GET function
+        # It should return an object of type 'QuerySet'
+        # and that object's length should be more than 0
         getGroupData = ObaseApi.Data.get()
-        assert(type(getGroupData) is QuerySet)
         assert(len(getGroupData._result_cache) > 0)
