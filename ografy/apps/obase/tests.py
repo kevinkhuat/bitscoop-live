9d40b98b3702ff9e62ca56b9e67f6dd20e7e7fdc
William Broza <hegemon.bill@gmail.com>
2014-11-29 12:13:15 -0800

    removed phone contact.

--- ografy/apps/obase/tests.py
+++ ografy/apps/obase/tests.py
@@ -1,11 +1,19 @@
 from django.test import SimpleTestCase, TransactionTestCase
+from django.core import serializers
 
-import json
+from ografy.apps.obase.api import Provider as ProviderApi, Signal as SignalApi
+from ografy.apps.obase.api import Data as DataApi, Event as EventApi, Message as MessageApi
+from ografy.apps.obase.documents import Message, Data, Event
+from ografy.apps.obase.models import Provider, Signal
+from ografy.apps.xauth.models import User
 
 
-class TestOBase(TransactionTestCase):
+class TestOBase(SimpleTestCase):
     fixtures = []
 
-    def test_OBase_Event(self):
+    # def test_OBase_Signal_Api(self):
+    #     test_user = User(email='test@test.test', handle='testy')
+    #     test_user.save()
+    #     test_signal = SignalApi.post(name='Facebook', backend_name='facebook', user=test_user)
+    #     test_json = serializers.serialize("json", test_signal)
 
-        pass
