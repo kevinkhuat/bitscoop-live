596f316a9fce62cb71835b5542400cfd1d1626c7
William Broza <hegemon.bill@gmail.com>
2014-12-23 13:51:29 -0800

    Fixed up list and sidebar in the main app

--- ografy/apps/core/static/core/js/list.js
+++ ografy/apps/core/static/core/js/list.js
@@ -14,66 +14,66 @@ function getCookie(name) {
     return cookieValue;
 }
 
-$(document).ready(function() {
-	var csrftoken = getCookie('csrftoken');
-
-	// filter items on button click
-	$('.list-item').click(function() {
-		$(this).siblings().removeClass('active');
-		$(this).toggleClass('active');
-
-		if ($(this).hasClass('active')) {
-			$('.information').removeClass('invisible');
-
-			var objectId = $(this).attr('id');
-			$.ajax({
-				url: '/opi/event/' + objectId,
-				type: 'GET',
-				dataType: 'json',
-				headers: {"X-CSRFToken": csrftoken}
-			}).done(function(data, xhr, response) {
-
-				$('.information-date-location .data').html(data.data);
-				$('.information-date-location .location').html(data.location);
-				$('.information-date-location .date').html(data.created);
-				console.log(data);
-				jsonMarkers["features"].push(
-					{
-						"geometry": {
-							"coordinates": data.location,
-							"type": "Point"
-						},
-						"properties": {
-							"description": data.provider_name,
-							"id": "marker-htbzzpcz1",
-							"marker-color": "#1087bf",
-							"marker-size": "large",
-							"marker-symbol": "telephone",
-							"title": "Call from Lisa",
-							"data-type": "Call",
-							"data-time": "5:31 pm",
-							"data-image-uri": "{% static 'demo/img/lisa-portrait.jpg' %}"
-						},
-						"type": "Feature"
-					});
-					var map = L.mapbox.map('map', 'liambroza.hl4bi8d0', {
-						zoomControl: false
-						}).setView(data.location, 16);
-					map.featureLayer = L.mapbox.featureLayer(jsonMarkers).addTo(map);
-
-			// find and store a variable reference to the list of filters
-					var filters = document.getElementById('filters');
-
-					var typesObj = {}, types = [];
-					var features = map.featureLayer.getGeoJSON().features;
-					for (var i = 0; i < features.length; i++) {
-						typesObj[features[i].properties['data-type']] = true;
-		}
-			});
-		}
-		else {
-			$('.information').addClass('invisible');
-		}
-	});
-
-});
+//$(document).ready(function() {
+//	var csrftoken = getCookie('csrftoken');
+//
+//	// filter items on button click
+//	$('.list-item').click(function() {
+//		$(this).siblings().removeClass('active');
+//		$(this).toggleClass('active');
+//
+//		if ($(this).hasClass('active')) {
+//			$('.information').removeClass('invisible');
+//
+//			var objectId = $(this).attr('id');
+//			$.ajax({
+//				url: '/opi/event/' + objectId,
+//				type: 'GET',
+//				dataType: 'json',
+//				headers: {"X-CSRFToken": csrftoken}
+//			}).done(function(data, xhr, response) {
+//
+//				$('.information-date-location .data').html(data.data);
+//				$('.information-date-location .location').html(data.location);
+//				$('.information-date-location .date').html(data.created);
+//				console.log(data);
+//				jsonMarkers["features"].push(
+//					{
+//						"geometry": {
+//							"coordinates": data.location,
+//							"type": "Point"
+//						},
+//						"properties": {
+//							"description": data.provider_name,
+//							"id": "marker-htbzzpcz1",
+//							"marker-color": "#1087bf",
+//							"marker-size": "large",
+//							"marker-symbol": "telephone",
+//							"title": "Call from Lisa",
+//							"data-type": "Call",
+//							"data-time": "5:31 pm",
+//							"data-image-uri": "{% static 'demo/img/lisa-portrait.jpg' %}"
+//						},
+//						"type": "Feature"
+//					});
+//					var map = L.mapbox.map('map', 'liambroza.hl4bi8d0', {
+//						zoomControl: false
+//						}).setView(data.location, 16);
+//					map.featureLayer = L.mapbox.featureLayer(jsonMarkers).addTo(map);
+//
+//			// find and store a variable reference to the list of filters
+//					var filters = document.getElementById('filters');
+//
+//					var typesObj = {}, types = [];
+//					var features = map.featureLayer.getGeoJSON().features;
+//					for (var i = 0; i < features.length; i++) {
+//						typesObj[features[i].properties['data-type']] = true;
+//		}
+//			});
+//		}
+//		else {
+//			$('.information').addClass('invisible');
+//		}
+//	});
+//
+//});
