7d36f1da162bc0f1d9e4890cc7849de4c3a40949
kbaran <barankyle@yahoo.com>
2015-01-09 15:39:27 -0800

    Got some of mapboxGL working.  Locations from JSON can be rendered on the map.

--- ografy/apps/core/static/core/js/list.js
+++ ografy/apps/core/static/core/js/list.js
@@ -258,6 +258,7 @@ $(document).ready(function() {
     //var list_detail = nunjucks.render("/ografy/apps/core/static/core/templates/list_detail.html");
     $('.list-detail').html(list_detail);
 
+
 	//Make sure that the sidebar is driven by Nunjucks templates
 	//When a click event happens on a list item, it updates the sidebar
 	//with the corresponding json data
@@ -269,51 +270,51 @@ $(document).ready(function() {
 		if ($(this).hasClass('active')) {
 			$('.information').removeClass('invisible');
 
-			var objectId = $(this).attr('id');
-			$.ajax({
-				url: '/opi/event/' + objectId,
-				type: 'GET',
-				dataType: 'json',
-				headers: {"X-CSRFToken": csrftoken}
-			}).done(function(data, xhr, response) {
-
-				$('.information-date-location .data').html(data.data);
-				$('.information-date-location .location').html(data.location);
-				$('.information-date-location .date').html(data.created);
-				console.log(data);
-				jsonMarkers["features"].push(
-					{
-						"geometry": {
-							"coordinates": data.location,
-							"type": "Point"
-						},
-						"properties": {
-							"description": data.provider_name,
-							"id": "marker-htbzzpcz1",
-							"marker-color": "#1087bf",
-							"marker-size": "large",
-							"marker-symbol": "telephone",
-							"title": "Call from Lisa",
-							"data-type": "Call",
-							"data-time": "5:31 pm",
-							"data-image-uri": "{% static 'demo/img/lisa-portrait.jpg' %}"
-						},
-						"type": "Feature"
-					});
-					var map = L.mapbox.map('map', 'liambroza.hl4bi8d0', {
-						zoomControl: false
-						}).setView(data.location, 16);
-					map.featureLayer = L.mapbox.featureLayer(jsonMarkers).addTo(map);
-
-			// find and store a variable reference to the list of filters
-					var filters = document.getElementById('filters');
-
-					var typesObj = {}, types = [];
-					var features = map.featureLayer.getGeoJSON().features;
-					for (var i = 0; i < features.length; i++) {
-						typesObj[features[i].properties['data-type']] = true;
-		}
-			});
+		//	var objectId = $(this).attr('id');
+		//	$.ajax({
+		//		url: '/opi/event/' + objectId,
+		//		type: 'GET',
+		//		dataType: 'json',
+		//		headers: {"X-CSRFToken": csrftoken}
+		//	}).done(function(data, xhr, response) {
+        //
+		//		$('.information-date-location .data').html(data.data);
+		//		$('.information-date-location .location').html(data.location);
+		//		$('.information-date-location .date').html(data.created);
+		//		console.log(data);
+		//		jsonMarkers["features"].push(
+		//			{
+		//				"geometry": {
+		//					"coordinates": data.location,
+		//					"type": "Point"
+		//				},
+		//				"properties": {
+		//					"description": data.provider_name,
+		//					"id": "marker-htbzzpcz1",
+		//					"marker-color": "#1087bf",
+		//					"marker-size": "large",
+		//					"marker-symbol": "telephone",
+		//					"title": "Call from Lisa",
+		//					"data-type": "Call",
+		//					"data-time": "5:31 pm",
+		//					"data-image-uri": "{% static 'demo/img/lisa-portrait.jpg' %}"
+		//				},
+		//				"type": "Feature"
+		//			});
+		//			var map = L.mapbox.map('map', 'liambroza.hl4bi8d0', {
+		//				zoomControl: false
+		//				}).setView(data.location, 16);
+		//			map.featureLayer = L.mapbox.featureLayer(jsonMarkers).addTo(map);
+        //
+		//	// find and store a variable reference to the list of filters
+		//			var filters = document.getElementById('filters');
+        //
+		//			var typesObj = {}, types = [];
+		//			var features = map.featureLayer.getGeoJSON().features;
+		//			for (var i = 0; i < features.length; i++) {
+		//				typesObj[features[i].properties['data-type']] = true;
+		//}
+		//	});
 		}
 		else {
 			$('.information').addClass('invisible');
