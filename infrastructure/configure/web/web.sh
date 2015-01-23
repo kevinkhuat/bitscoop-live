764c4830fba69ecbf67bff7d53f8163278e93f4d
Steven Berry <sberry@ografy.io>
2015-01-22 18:30:32 -0800

    Updating web configuration script.

--- infrastructure/configure/web/web.sh
+++ infrastructure/configure/web/web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-WD=`dirname ${BASH_SOURCE}`
+WD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source ${WD}/../parseargs.sh
 source ${WD}/../baseline.sh
 
@@ -20,14 +20,11 @@ esac
 sudo yum install -y --disablerepo=* --enablerepo=ografy stunnel
 sudo -u ${CUSR} ${WD}/../../scripts/installation/Python-3.4.2.sh install
 sudo -u ${CUSR} ${WD}/../../scripts/installation/passenger-4.0.53.sh install
+sudo -u ${CUSR} ${WD}/../../scripts/installation/ografy-0.2.0.sh install
 
 
 # Set appropriate default permissions.
 umask 022
-
-
-# Create log directory structure.
-sudo mkdir -p /var/log/nginx/ografy.io/{static,www}
 
 
 # Copy configuration files.
