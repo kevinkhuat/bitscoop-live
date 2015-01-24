e6709f38214745c71e05a097d21eee38c28c33b4
William Broza <hegemon.bill@gmail.com>
2015-01-23 17:47:09 -0800

    Tons of fixes to signal association and small fixes to up js

--- ografy/apps/tastydata/views.py
+++ ografy/apps/tastydata/views.py
@@ -39,6 +39,10 @@ class APIView(BaseAPIView):
                 query = request.query_params['filter']
                 # add filter query
                 request.query_filter = parse_filter(query, expression_class=self.Meta.Q)
+            if 'search' in request.query_params:
+                query = request.query_params['search']
+                # add search query
+                # TODO: Add full text search hook for all text in DB
             else:
                 request.query_filter = self.Meta.Q()
         else:
--- ografy/apps/xauth/pipeline.py
+++ ografy/apps/xauth/pipeline.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import redirect
 
 from social.pipeline.partial import partial
@@ -20,11 +21,21 @@ def require_email(strategy, details, user=None, is_new=False, *args, **kwargs):
 
 def associate_user_and_signal(backend, uid, user=None, social=None, *args, **kwargs):
     association = associate_user(backend=backend, uid=uid, user=user, social=social, **kwargs)
-    provider = Provider.objects.filter(backend=backend)
-    social = Signal(
-        user=association.user,
-        provider=provider,
-        name=backend,
-        psa_backend_id=association.social.id
-        )
-    social.save()
+    if association:
+        user = association['user']
+        social = association['social']
+        provider = Provider.objects.get(backend_name=backend.name)
+        signal = Signal(
+            user=user,
+            provider=provider,
+            name=backend.name,
+            psa_backend_uid=social.uid,
+            connected=True,
+            complete=False,
+            enabled=False,
+            created=datetime.now(),
+            updated=datetime.now()
+            )
+        signal.save()
+
+    return association
