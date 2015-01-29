835fbae3fb7ba849a2681fe656f6e10f9de162b1
Steven Berry <sberry@ografy.io>
2015-01-29 13:40:07 -0800

    Minor revisions to rdb configuration script.

--- infrastructure/configure/rdb/rdb.sh
+++ infrastructure/configure/rdb/rdb.sh
@@ -6,20 +6,20 @@ WD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd /
 
-sudo yum install -y postgresql-devel
 
 #Install the PostgreSQL 9.3 YUM repository and then install the PostgreSQL packages
+sudo yum install -y postgresql-devel
 sudo yum install -y http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-redhat93-9.3-1.noarch.rpm
 sudo yum install -y postgresql93-server postgresql93-contrib
 
-#Set PostgreSQL to automatically start and be initialized
 
-sudo service postgresql-9.3 initdb || /usr/pgsql-9.3/bin/postgresql93-setup initdb
 
 umask 022
 sudo cp -rv ${WD}/etc ${WD}/var /
 
 
+#Set PostgreSQL to automatically start and be initialized
+sudo service postgresql-9.3 initdb || /usr/pgsql-9.3/bin/postgresql93-setup initdb
 sudo chkconfig postgresql-9.3 on
 sudo service postgresql-9.3 start
 
