199fec9f55b6d2ad199b24cae51cad351cbcbb3c
Steven Berry <sberry@ografy.io>
2015-02-13 17:56:27 -0800

    Submitting a hack fix for running the collect static on a local machine that doesn't necessarily have a compliant version of PostgreSQL installed or the appropriate folder structure for logging established.

---
+++ ografy/settings/production/deploy.py
@@ -0,0 +1,6 @@
+from ografy.settings.production import *
+
+
+# Allows running the collectstatic management command without Django throwing an error that the log file doesn't exist.
+LOGGING = {}
+DATABASES = {}
