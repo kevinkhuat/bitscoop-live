d97319661c53e02b90af2957fea128ccd43b32a8
kbaran <barankyle@yahoo.com>
2015-03-25 19:06:16 -0700

    Mobile layout fixes for main app and settings page.

--- ografy/apps/core/static/core/js/main/require_load.js
+++ ografy/apps/core/static/core/js/main/require_load.js
@@ -6,27 +6,52 @@ requirejs.config({
 	},
 	shim: {
 		'main/base': {
-			deps: ['main/detail', 'main/views/list', 'main/views/map', 'main/utls/cache',
-					'main/utls/model', 'main/utls/mapUtil', 'main/utls/session', 'main/views/list',
+			deps: ['main/detail',
+					'main/views/list',
+					'main/views/map',
+					'main/utls/cache',
+					'main/utls/model',
+					'main/utls/mapUtil',
+					'main/utls/session',
+					'main/views/list',
 					'main/filter/search']
 		},
 		'main/detail': {
-			deps: ['main/utls/cache', 'main/utls/mapUtil', 'main/utls/model', 'main/utls/session']
+			deps: ['main/utls/cache',
+					'main/utls/mapUtil',
+					'main/utls/model',
+					'main/utls/session']
 		},
 		'main/list': {
-			deps: ['main/utls/cache', 'main/utls/mapUtil', 'main/utls/model', 'main/utls/session']
+			deps: ['main/utls/cache',
+					'main/utls/mapUtil',
+					'main/utls/model',
+					'main/utls/session']
 		},
 		'main/map': {
-			deps: ['main/utls/cache', 'main/utls/mapUtil', 'main/utls/model', 'main/utls/session']
+			deps: ['main/utls/cache',
+					'main/utls/mapUtil',
+					'main/utls/model',
+					'main/utls/session']
 		}
 	}
 });
 
 // Start the main app logic.
-requirejs(['main/utls/cache', 'main/utls/mapUtil', 'main/utls/model', 'main/utls/session',
-			'main/utls/url', 'main/detail', 'main/views/list', 'main/views/map', 'main/filter/search',
-			'main/filter/objects/data/metadata', 'main/filter/objects/event/metadata',
-			'main/filter/objects/message/metadata',  'main/base'],
+requirejs(['main/utls/cache',
+			'main/utls/mapUtil',
+			'main/utls/model',
+			'main/utls/session',
+			'main/utls/url',
+			'main/detail',
+			'main/views/list',
+			'main/views/map',
+			'main/filter/search',
+			'main/filter/objects/data/metadata',
+			'main/filter/objects/event/metadata',
+			'main/filter/objects/message/metadata',
+			'main/base'],
+
 	function() {
 		$(document).ready(function() {
 			var base = baseView();
