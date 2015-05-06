acb940d9073ec046d4795a140568c71bd041238d
William Broza <hegemon.bill@gmail.com>
2015-05-06 11:09:03 -0700

    Removed executable commands keeping only the env variables

--- signals_env.sh
+++ signals_env.sh
@@ -1,95 +1,90 @@
-#!/usr/bin/env bash
+# Amazon
 
-$EDITOR ~/.bashrc
-#add lines at the bottom of the file:
+export ografy.settings.psa.SOCIAL_AUTH_AMAZON_KEY = 'amzn1.application-oa2-client.eeb66ec1dc3c47529c28f85ed979ec02'
+export ografy.settings.psa.SOCIAL_AUTH_AMAZON_SECRET = 'db192c908e6d7df30af44aceb41f3521056975cd52810db066e70a204b9ddac9'
 
-    # Amazon
+# Dropbox
 
-    export ografy.settings.psa.SOCIAL_AUTH_AMAZON_KEY = 'amzn1.application-oa2-client.eeb66ec1dc3c47529c28f85ed979ec02'
-    export ografy.settings.psa.SOCIAL_AUTH_AMAZON_SECRET = 'db192c908e6d7df30af44aceb41f3521056975cd52810db066e70a204b9ddac9'
+export ografy.settings.psa.SOCIAL_AUTH_DROPBOX_OAUTH2_KEY = '89dmvfe194u7nfl'
+export ografy.settings.psa.SOCIAL_AUTH_DROPBOX_OAUTH2_SECRET = 'ix3u2xryo4bs8b9'
 
-    # Dropbox
+# Facebook App Graph API Auth
 
-    export ografy.settings.psa.SOCIAL_AUTH_DROPBOX_OAUTH2_KEY = '89dmvfe194u7nfl'
-    export ografy.settings.psa.SOCIAL_AUTH_DROPBOX_OAUTH2_SECRET = 'ix3u2xryo4bs8b9'
+export ografy.settings.psa.SOCIAL_AUTH_FACEBOOK_KEY = '296338707043090'
+export ografy.settings.psa.SOCIAL_AUTH_FACEBOOK_SECRET = '358004e6e35ed720761aaff7b487375d'
 
-    # Facebook App Graph API Auth
+# Faceook Auth
 
-    export ografy.settings.psa.SOCIAL_AUTH_FACEBOOK_KEY = '296338707043090'
-    export ografy.settings.psa.SOCIAL_AUTH_FACEBOOK_SECRET = '358004e6e35ed720761aaff7b487375d'
+export ografy.settings.psa.SOCIAL_AUTH_FACEBOOK_APP_KEY = '296338707043090'
+export ografy.settings.psa.SOCIAL_AUTH_FACEBOOK_APP_SECRET = '358004e6e35ed720761aaff7b487375d'
 
-    # Faceook Auth
+# FitBit
 
-    export ografy.settings.psa.SOCIAL_AUTH_FACEBOOK_APP_KEY = '296338707043090'
-    export ografy.settings.psa.SOCIAL_AUTH_FACEBOOK_APP_SECRET = '358004e6e35ed720761aaff7b487375d'
+export ografy.settings.psa.SOCIAL_AUTH_FITBIT_KEY = '72007be9664f4f13ac481335b001c520'
+export ografy.settings.psa.SOCIAL_AUTH_FITBIT_SECRET = 'a5aed45ae0f440c29f19eb4e9c2e0493'
 
-    # FitBit
+# Foursquare
 
-    export ografy.settings.psa.SOCIAL_AUTH_FITBIT_KEY = '72007be9664f4f13ac481335b001c520'
-    export ografy.settings.psa.SOCIAL_AUTH_FITBIT_SECRET = 'a5aed45ae0f440c29f19eb4e9c2e0493'
+export ografy.settings.psa.SOCIAL_AUTH_FOURSQUARE_KEY = 'PSHPVDWN0NCQCUUIPUC3CQBZCDMOY4B3AYOAHGAAW1Y0V0DT'
+export ografy.settings.psa.SOCIAL_AUTH_FOURSQUARE_SECRET = 'EGTRPVVFLDKA4QNLN5ACS43V2B5KLSD0FSUOPYTKX3FFPA21'
 
-    # Foursquare
+# Github Auth
 
-    export ografy.settings.psa.SOCIAL_AUTH_FOURSQUARE_KEY = 'PSHPVDWN0NCQCUUIPUC3CQBZCDMOY4B3AYOAHGAAW1Y0V0DT'
-    export ografy.settings.psa.SOCIAL_AUTH_FOURSQUARE_SECRET = 'EGTRPVVFLDKA4QNLN5ACS43V2B5KLSD0FSUOPYTKX3FFPA21'
+export ografy.settings.psa.SOCIAL_AUTH_GITHUB_KEY = 'af9f6b46f5b02049d4f4'
+export ografy.settings.psa.SOCIAL_AUTH_GITHUB_SECRET = '794ea7c3067051aab7f9470fdfe099703047b071'
 
-    # Github Auth
+# Google
 
-    export ografy.settings.psa.SOCIAL_AUTH_GITHUB_KEY = 'af9f6b46f5b02049d4f4'
-    export ografy.settings.psa.SOCIAL_AUTH_GITHUB_SECRET = '794ea7c3067051aab7f9470fdfe099703047b071'
+export ografy.settings.psa.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1047735864420-j7nr84qtkag5o2pno2v19c8uv00utsq0.apps.googleusercontent.com'
+export ografy.settings.psa.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'kEnHSEacGJqX7JQOk1BQj1gR'
 
-    # Google
+# Instagram
 
-    export ografy.settings.psa.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1047735864420-j7nr84qtkag5o2pno2v19c8uv00utsq0.apps.googleusercontent.com'
-    export ografy.settings.psa.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'kEnHSEacGJqX7JQOk1BQj1gR'
+export ografy.settings.psa.SOCIAL_AUTH_INSTAGRAM_KEY = 'cc0103f57968493f87139269e452df61'
+export ografy.settings.psa.SOCIAL_AUTH_INSTAGRAM_SECRET = 'ce402cb985c24f5a818207316f806f50'
 
-    # Instagram
+# LinkedIn
 
-    export ografy.settings.psa.SOCIAL_AUTH_INSTAGRAM_KEY = 'cc0103f57968493f87139269e452df61'
-    export ografy.settings.psa.SOCIAL_AUTH_INSTAGRAM_SECRET = 'ce402cb985c24f5a818207316f806f50'
+# API Key:                      755c6aq5wo070o
+# Secret Key:                   ZQS9Gsgw0snBotYB
+export ografy.settings.psa.SOCIAL_AUTH_LINKEDIN_OAUTH2_KEY = '588631a0-1be8-4570-9413-7be28a2aefa0'
+export ografy.settings.psa.SOCIAL_AUTH_LINKEDIN_OAUTH2_SECRET = '0f4b82e3-1aa8-4375-b5cf-21be8b30b0eb'
 
-    # LinkedIn
+# Reddit
 
-    # API Key:                      755c6aq5wo070o
-    # Secret Key:                   ZQS9Gsgw0snBotYB
-    export ografy.settings.psa.SOCIAL_AUTH_LINKEDIN_OAUTH2_KEY = '588631a0-1be8-4570-9413-7be28a2aefa0'
-    export ografy.settings.psa.SOCIAL_AUTH_LINKEDIN_OAUTH2_SECRET = '0f4b82e3-1aa8-4375-b5cf-21be8b30b0eb'
+# developers                    hegemonbill
+export ografy.settings.psa.SOCIAL_AUTH_REDDIT_KEY = 'yVmBYysV38qrHQ'
+export ografy.settings.psa.SOCIAL_AUTH_REDDIT_SECRET = 'kPQGNdbEQyS8hsDUk21GRGosKko'
 
-    # Reddit
+# Spotify
 
-    # developers                    hegemonbill
-    export ografy.settings.psa.SOCIAL_AUTH_REDDIT_KEY = 'yVmBYysV38qrHQ'
-    export ografy.settings.psa.SOCIAL_AUTH_REDDIT_SECRET = 'kPQGNdbEQyS8hsDUk21GRGosKko'
+export ografy.settings.psa.SOCIAL_AUTH_SPOTIFY_OAUTH2_KEY = '1495263d3fbe4fc59f69a9a9af9f9281'
+export ografy.settings.psa.SOCIAL_AUTH_SPOTIFY_OAUTH2_SECRET = '0aacb16969eb43a2a3a019bb2ae091d2'
 
-    # Spotify
+# StackOverflow
 
-    export ografy.settings.psa.SOCIAL_AUTH_SPOTIFY_OAUTH2_KEY = '1495263d3fbe4fc59f69a9a9af9f9281'
-    export ografy.settings.psa.SOCIAL_AUTH_SPOTIFY_OAUTH2_SECRET = '0aacb16969eb43a2a3a019bb2ae091d2'
+export ografy.settings.psa.SOCIAL_AUTH_STACKOVERFLOW_KEY = 'FNiAzOP9EO2RXrfl)ttOXA(('
+export ografy.settings.psa.SOCIAL_AUTH_STACKOVERFLOW_SECRET = 'TduWUXJnEZlr6YpGnxy7CA(('
+export ografy.settings.psa.SOCIAL_AUTH_STACKOVERFLOW_API_KEY = '3718'
 
-    # StackOverflow
+# Steam
 
-    export ografy.settings.psa.SOCIAL_AUTH_STACKOVERFLOW_KEY = 'FNiAzOP9EO2RXrfl)ttOXA(('
-    export ografy.settings.psa.SOCIAL_AUTH_STACKOVERFLOW_SECRET = 'TduWUXJnEZlr6YpGnxy7CA(('
-    export ografy.settings.psa.SOCIAL_AUTH_STACKOVERFLOW_API_KEY = '3718'
+export ografy.settings.psa.SOCIAL_AUTH_STEAM_API_KEY = 'F807EEE21B2D47BB0AF8AB92483F3CCC'
 
-    # Steam
+# Tumblr
 
-    export ografy.settings.psa.SOCIAL_AUTH_STEAM_API_KEY = 'F807EEE21B2D47BB0AF8AB92483F3CCC'
+export ografy.settings.psa.SOCIAL_AUTH_TUMBLR_KEY = 'aTWEANrCihZmUyfVnIlcCGSNSJEc4pQcGC3WHFQGK9DSY9CYya'
+export ografy.settings.psa.SOCIAL_AUTH_TUMBLR_SECRET = 'Uzpi2JvCdS1Gtr5GhGkHIIyXcaE6ylyU4HhuCJhyG7tKJvq7jy'
 
-    # Tumblr
+# Twitter
 
-    export ografy.settings.psa.SOCIAL_AUTH_TUMBLR_KEY = 'aTWEANrCihZmUyfVnIlcCGSNSJEc4pQcGC3WHFQGK9DSY9CYya'
-    export ografy.settings.psa.SOCIAL_AUTH_TUMBLR_SECRET = 'Uzpi2JvCdS1Gtr5GhGkHIIyXcaE6ylyU4HhuCJhyG7tKJvq7jy'
+# Owner:                        hegemonbill
+# Owner ID:                     158017896
+export ografy.settings.psa.SOCIAL_AUTH_TWITTER_KEY = 'HmrZvp1vaHU1Fb7sIg7KCL0Rs'
+export ografy.settings.psa.SOCIAL_AUTH_TWITTER_SECRET = 'UnyUntGyjnGUcqpZBIpZWNsPEzG0YezaGbIhgKEWQn7n54N3dr'
 
-    # Twitter
+# Vimeo
 
-    # Owner:                        hegemonbill
-    # Owner ID:                     158017896
-    export ografy.settings.psa.SOCIAL_AUTH_TWITTER_KEY = 'HmrZvp1vaHU1Fb7sIg7KCL0Rs'
-    export ografy.settings.psa.SOCIAL_AUTH_TWITTER_SECRET = 'UnyUntGyjnGUcqpZBIpZWNsPEzG0YezaGbIhgKEWQn7n54N3dr'
-
-    # Vimeo
-
-    # Authorization:              Basic ZmM1MDE1MDBhMDg0Y2FmMTI5NTA3OTY1NDU4OWNkYjQ4NzllZDIxYjo0YTAxYzk2YzFhZjgyNWNlMTdhZjIyYTIzN2E4NjI0YzgxNTlmMzEx
-    export ografy.settings.psa.SOCIAL_AUTH_VIMEO_KEY = 'fc501500a084caf1295079654589cdb4879ed21b'
-    export ografy.settings.psa.SOCIAL_AUTH_VIMEO_SECRET = '4a01c96c1af825ce17af22a237a8624c8159f311'
+# Authorization:              Basic ZmM1MDE1MDBhMDg0Y2FmMTI5NTA3OTY1NDU4OWNkYjQ4NzllZDIxYjo0YTAxYzk2YzFhZjgyNWNlMTdhZjIyYTIzN2E4NjI0YzgxNTlmMzEx
+export ografy.settings.psa.SOCIAL_AUTH_VIMEO_KEY = 'fc501500a084caf1295079654589cdb4879ed21b'
+export ografy.settings.psa.SOCIAL_AUTH_VIMEO_SECRET = '4a01c96c1af825ce17af22a237a8624c8159f311'
